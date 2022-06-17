4e0a632f35ce12e4c1f6ff8082335c8ab9f36bb6
toshiyuki <toshiyuki@home>
2022-06-17 10:00:44 +0900

    javadoc追加

--- emarf-job/src/main/java/jp/co/golorp/emarf/job/BaseJob.java
+++ emarf-job/src/main/java/jp/co/golorp/emarf/job/BaseJob.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 golorp
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package jp.co.golorp.emarf.job;
 
 import java.time.LocalDateTime;
@@ -14,20 +30,17 @@ import jp.co.golorp.emarf.sql.Connections;
 /**
  * バッチ基底クラス
  *
- * @author toshiyuki
- *
+ * @author golorp
  */
 public abstract class BaseJob extends BaseProcess {
 
-    /**
-     * logger
-     */
+    /** logger */
     private static final Logger LOG = LoggerFactory.getLogger(BaseJob.class);
 
     /**
      * 事前処理
      *
-     * @param args
+     * @param args 起動引数
      */
     public void run(final String[] args) {
 
@@ -70,9 +83,9 @@ public abstract class BaseJob extends BaseProcess {
     /**
      * 主処理
      *
-     * @param now
-     * @param id
-     * @param args
+     * @param now システム日時
+     * @param id 起動ID
+     * @param args 起動引数
      */
     public abstract void running(LocalDateTime now, String id, String[] args);
 
