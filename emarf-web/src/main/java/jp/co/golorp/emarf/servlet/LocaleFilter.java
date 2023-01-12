2af08f153a77cd25f4e0b421d9daf3e9d67854b1
ktc-fukuo <t_fukuo@kyototool.co.jp>
2023-01-12 16:07:21 +0900

    エクセルダウンロードを同ウィンドウ内に変更

--- emarf-web/src/main/java/jp/co/golorp/emarf/servlet/LocaleFilter.java
+++ emarf-web/src/main/java/jp/co/golorp/emarf/servlet/LocaleFilter.java
@@ -17,7 +17,6 @@ limitations under the License.
 package jp.co.golorp.emarf.servlet;
 
 import java.io.IOException;
-import java.util.Locale;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -51,10 +50,10 @@ public class LocaleFilter implements Filter {
             throws IOException, ServletException {
 
         // messageのi18n対応
-        Locale locale = Locales.get();
-        if (!locale.getLanguage().equals(request.getLocale().getLanguage())) {
-            Locales.set(request.getLocale());
-        }
+        //        Locale locale = Locales.get();
+        //        if (!locale.getLanguage().equals(request.getLocale().getLanguage())) {
+        Locales.set(request.getLocale());
+        //        }
 
         chain.doFilter(request, response);
     }
