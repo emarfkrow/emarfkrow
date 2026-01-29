f2e3cd5035f43079f45ae2b3b46721c96e3fb666
ToshiyukiFukuo <t.fukuo@gmail.com>
2026-01-29 21:20:15 +0900

    ファイル整理

---
+++ emarf-com/src/main/java/jp/co/golorp/emarf/App.java
@@ -0,0 +1,23 @@
+package jp.co.golorp.emarf;
+
+/**
+ * Hello world!
+ *
+ */
+public final class App {
+
+    /**
+     * エントリポイント
+     *
+     * @param args コマンドライン引数
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * プライベートコンストラクタ
+     */
+    private App() {
+    }
+}
---
+++ emarf-com/src/test/java/jp/co/golorp/emarf/AppTest2.java
@@ -0,0 +1,16 @@
+package jp.co.golorp.emarf;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest2 {
+
+    /**
+     * test
+     */
+    @Test
+    void test() {
+        Assertions.fail("まだ実装されていません");
+    }
+
+}
